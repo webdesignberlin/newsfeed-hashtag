04426db554f702f49b6395e8763f056054166824
Michael Gerstmann <mg@bytepark.de>
2014-10-02 16:34:16 +0200

    remove header animation initialisation

--- web/assets-dev/js/main.js
+++ web/assets-dev/js/main.js
@@ -1,27 +1,21 @@
-function init() {
-    window.addEventListener('scroll', function(e){
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 100,
-            header = $('.header-background');
-        if (distanceY > shrinkOn) {
-            header.addClass('header-background--small');
-        } else {
-            if (header.has('.header-background--small')) {
-                header.removeClass('header-background--small');
-            }
-        }
-    });
-}
-window.onload = init();
-
 ;(function( $, window, document, undefined ) {
     // well, it's pretty strict...
     'use strict';
 
-    //console.debug('Hallo Welt.');
-
-    // var x = $('#gibtsnicht');
-
-    //console.debug( x );
+    function init() {
+        window.addEventListener('scroll', function(e){
+            var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+                shrinkOn = 100,
+                header = $('.header-background');
+            if (distanceY > shrinkOn) {
+                header.addClass('header-background--small');
+            } else {
+                if (header.has('.header-background--small')) {
+                    header.removeClass('header-background--small');
+                }
+            }
+        });
+    }
+    //window.onload = init();
 
 })(jQuery,window,document);
